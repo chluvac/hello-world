63718fa88b8894f9147791292867ea5b47008bfe
chluvac <valibuk0@gmail.com>
2014-09-09 18:03:55 +0200

    první commit

---
+++ součet.php
@@ -0,0 +1,31 @@
+<html>
+<?php
+
+if(!isset($cislo)){
+$cislo = 0;
+}
+
+$soucet = 0;
+
+function delitelne3($cislo){
+	if(is_int($cislo/3)){
+		$soucet = $soucet + $cislo;
+		$delitelne3 = TRUE;
+	} else ($delitelne3 = FALSE;)
+}
+
+function delitelne5($cislo){
+	if(is_int($cislo/5)){
+		$soucet = $soucet + $cislo;
+	}
+}
+
+for($cislo; $cislo < 1000; $cislo++){
+	delitelne3($cislo);
+	if($delitelne3 == FALSE){
+		delitelne5;
+	}
+}
+
+echo($cislo);
+</html>
